97442efb7572bdae9cadb304ad763c1dc931afb8
sohanur-shanto <sohanur.shanto@northsouth.edu>
2021-11-10 13:35:36 +0600

    model

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
-# Create your models here.
-#ok
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+
+    def __str__(self):
+        return self.title
